9bba5ce138958ebb34fe8aa6afaed04c2d739b7c
Reza Praditya <29914382+rzdev@users.noreply.github.com>
2020-06-29 20:12:37 +0700

    add console.log when request to api

--- src/api/hooks/useFilm.ts
+++ src/api/hooks/useFilm.ts
@@ -2,6 +2,7 @@ import {useQuery} from 'react-query';
 import axios from 'axios';
 
 const getFilmById = async (_key: string, FilmId: number) => {
+  console.log('getFilmById');
   const {data} = await axios.get(`https://swapi.dev/api/films/${FilmId}`);
   return data;
 };
--- src/api/hooks/useFilms.ts
+++ src/api/hooks/useFilms.ts
@@ -2,6 +2,7 @@ import {useQuery} from 'react-query';
 import axios from 'axios';
 
 const getFilms = async () => {
+  console.log('getFilms');
   const {data} = await axios.get('https://swapi.dev/api/films');
   return data;
 };
